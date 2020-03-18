d008409dd61dab3b49414ad4f5729fcc8a153431
Владислав Бодин <vladislavbodin@MacBook-Pro-Vladislav.local>
2020-03-18 12:37:27 +0300

    Завершает работу над шапкой

---
+++ build/js/menu-nav.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var headerNav = document.querySelector('.page-header__nav-wrapper');
+var closeBtn = document.querySelector('.page-header__toggle');
+
+headerNav.classList.remove('page-header__nav-wrapper--nojs');
+
+closeBtn.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  closeBtn.classList.toggle('page-header__toggle--close');
+  headerNav.classList.toggle('page-header__nav-wrapper--open');
+});
